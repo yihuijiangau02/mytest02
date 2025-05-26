fdc7149012a770439aeeff021301387f7b8b90ef
yihui <yihui@yihuis-iMac.local>
2025-05-26 04:22:16 -0700

    Your commit message here

---
+++ mytest02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mytest02
+//
+//  Created by yihui on 26/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
